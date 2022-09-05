ee7db4edd78cef0fd80550fb1a1109a9c9934f21
Dmitry <spitsynfilmeffect@gmail.com>
2022-09-05 21:10:05 +0300

    first task is done

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Добро пожаловать в программу сравнения двух чисел");
+Console.WriteLine("Введите первое число");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+if (FirstNumber>SecondNumber)
+{System.Console.WriteLine($"Число {FirstNumber} больше числа {SecondNumber}");
+
+}
+if (FirstNumber==SecondNumber){
+    System.Console.WriteLine("Оба числа равны");
+}
+else
+System.Console.WriteLine($"Число {FirstNumber} меньше числа {SecondNumber}");
