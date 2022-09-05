c91cb9845ec2962dda48d25ed5da5dffae74a189
Dmitry <spitsynfilmeffect@gmail.com>
2022-09-05 21:27:54 +0300

    second task is done

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Добро пожаловать в программу нахождения максимального из трех числа ");
+Console.WriteLine("Введите первое число");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int ThirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = FirstNumber;
+if (max<SecondNumber){
+    max=SecondNumber;
+}
+if (max<ThirdNumber){
+    max=ThirdNumber;
+}
+System.Console.WriteLine($"Большее число -> {max}");
