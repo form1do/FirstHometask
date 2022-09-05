dcd3c0d53ee97df0b036b38e0cdcc4f4c57402ae
Dmitry <spitsynfilmeffect@gmail.com>
2022-09-05 21:35:18 +0300

    third task is done

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Добро пожаловать в программу проверки на четность");
+System.Console.WriteLine("ВВедите число");
+int UserNumber = Convert.ToInt32(Console.ReadLine());
+if (UserNumber % 2 == 0){
+    System.Console.WriteLine($"Число {UserNumber} -> четное");
+}
+else
+{
+    System.Console.WriteLine($"Число {UserNumber} -> нечетное");
+}
